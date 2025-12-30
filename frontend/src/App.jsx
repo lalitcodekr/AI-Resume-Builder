eb78943eb99c86857abfd44621f742d9073d4172
venkata-harish28 <harishmartha19@gmail.com>
2025-12-30 21:28:14 +0530

    Landing page implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import TemplatesPage from './pages/TemplatesPage'
-import BuilderPage from './pages/Builder'
-import LoginPage from './pages/login'
-import RegisterPage from './pages/Register'
-import ForgotPasswordPage from './pages/ForgotPassword'
-import TemplateEditor from './pages/TemplateEditor'
-import NotFound from './pages/NotFound'
-import Contact from './pages/Contact'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
+import Home from "./pages/Home";
+import TemplatesPage from "./pages/TemplatesPage";
+import BuilderPage from "./pages/Builder";
+import LoginPage from "./pages/login";
+import RegisterPage from "./pages/Register";
+import ForgotPasswordPage from "./pages/ForgotPassword";
+import TemplateEditor from "./pages/TemplateEditor";
+import Contact from "./pages/Contact";
+import HelpCenter from "./pages/HelpCenter";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -20,16 +20,17 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/templates" element={<TemplatesPage />} />
           <Route path="/templates/:id" element={<TemplateEditor />} />
+          <Route path="/builder" element={<BuilderPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-          <Route path="/builder" element={<BuilderPage />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/help-center" element={<HelpCenter />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
