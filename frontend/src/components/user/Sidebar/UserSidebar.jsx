8a951518d1430f62cbb078a5823ac011da925837
Pyda Lalit Kumar <lalit.23bce7809@vitapstudent.ac.in>
2026-02-27 22:11:55 +0530

    Hamburger update in mobile view

--- frontend/src/components/user/Sidebar/UserSidebar.jsx
+++ frontend/src/components/user/Sidebar/UserSidebar.jsx
@@ -115,7 +115,7 @@ export default function UserSidebar() {
   return (
     <>
       {/* Toggle Buttons */}
-      <div className="fixed md:top-4 top-5 md:left-4 left-2 z-[60] flex gap-2">
+      <div className="fixed md:top-4 top-5 md:left-4 left-2 z-[1000] flex gap-2">
         <button
           onClick={() => setIsMobileOpen(!isMobileOpen)}
           className="md:hidden"
@@ -172,7 +172,7 @@ export default function UserSidebar() {
                     <motion.span
                       initial={{ scale: 0 }}
                       animate={{ scale: 1 }}
-                      className={`${isCollapsed ? 'absolute -top-1 -right-1' : 'ml-auto'} inline-flex items-center justify-center w-5 h-5 text-xs font-bold text-white rounded-full ${active ? 'bg-yellow-400' : 'bg-yellow-400'}`}
+                      className={`${isCollapsed ? "absolute -top-1 -right-1" : "ml-auto"} inline-flex items-center justify-center w-5 h-5 text-xs font-bold text-white rounded-full ${active ? "bg-yellow-400" : "bg-yellow-400"}`}
                     >
                       {item.badge}
                     </motion.span>
